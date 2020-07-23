89300553aa665031dc177b4ee34b5709383cf9aa
kamile17 <kamilen17@gmail.com>
2020-07-23 23:29:23 +0300

    Make dev env work again

--- taskmanagementsystem/settings.py
+++ taskmanagementsystem/settings.py
@@ -76,15 +76,18 @@ WSGI_APPLICATION = 'taskmanagementsystem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+
+if os.getenv('APP_ENV') == 'production':
+    DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-'''
-DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
